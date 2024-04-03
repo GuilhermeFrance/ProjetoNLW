1c668d2bc7bc1bd51e5a2195180b4b3d4da03795
Guilherme France <guilhermefrance9@gmail.com>
2024-04-03 18:23:19 -0300

    primeiro commit

--- projetocheckin/src/components/attendee-list.tsx
+++ projetocheckin/src/components/attendee-list.tsx
@@ -2,9 +2,43 @@ import { Search, MoreHorizontal, ChevronsLeft, ChevronLeft, ChevronRight, } from
 import { IconButton } from './icon-button'
 import { Table } from './table/table'
 import { TableHeader } from './table/table-header'
+import  dayjs from 'dayjs'
+import  'dayjs/Locale/pt-br'
+import relativeTime from 'dayjs/plugin/relativeTime'
 import { TableCell } from './table/table-cell'
 import { TableRow } from './table/table-row'
+import { attendees } from '../data/attendees'
+import { useState } from 'react'
+
+dayjs.extend(relativeTime)
+dayjs.locale('pt-br')
+
+
 export function AttendeeList() {
+ 
+const totalPages = Math.ceil(attendees.length /10)
+
+function goToFirstPage(){
+
+        setPage(1)
+}    
+
+function goToNextPage(){
+
+   setPage(page+1)
+}
+
+function goToPreviousPage(){
+
+    setPage(page-1)
+}
+function goToLastPage(){
+
+    setPage(totalPages)
+}
+
+
+const [page, setPage] = useState(1) 
 
     return (
         <div className='flex flex-col gap-4'>
@@ -20,7 +54,7 @@ export function AttendeeList() {
                 <thead>
                     <tr className='border-b border-white/10'>
                             <TableHeader style={{width: 48}} className='py-3 px-4 text-sm font-semibold text-left'> 
-                                <input type="checkbox" className='size-4 bg-black/20 rounded border border-white/10' />
+                                <input type="checkbox" className='size-4 bg-black/20 rounded border border-white/10  checked:bg-orange-400 ' />
                             </TableHeader>
                             <TableHeader >Codigo</TableHeader>
                             <TableHeader >Participantes</TableHeader>
@@ -30,21 +64,21 @@ export function AttendeeList() {
                     </tr>  
                 </thead>
                 <tbody>
-                    {Array.from({length: 8}).map((_, i) => {
+                    {attendees.slice((page - 1) * 10,page * 10).map((attendee) => {
                         return (
-                            <TableRow key={i}>
+                            <TableRow key={attendee.id}>
                         <TableCell> <input type="checkbox" className='size-4 bg-black/20 rounded border border-white/10 checked:bg-orange-400' /></TableCell>
-                        <TableCell>321312123</TableCell>
+                        <TableCell>{attendee.id}</TableCell>
                         <TableCell>
                             <div>
                                   <div className='flex flex-col gap-1'>
-                                         <span className='font-semibold text-white'>Guilherme France</span>
-                                         <span>guilhermefrance0@gmail.com</span>
+                                         <span className='font-semibold text-white'>{attendee.name}</span>
+                                         <span>{attendee.email}</span>
                                     </div>
                             </div>
                         </TableCell>
-                        <TableCell>7 dias atrás</TableCell>
-                        <TableCell>3 dias atrás</TableCell>
+                        <TableCell>{dayjs().to(attendee.createdAt)}</TableCell>
+                        <TableCell>{dayjs().to(attendee.checkedInAt)}</TableCell>
                         <TableCell>
                             
                             <IconButton transparent={true}> 
@@ -60,27 +94,27 @@ export function AttendeeList() {
                 <tfoot>
                     <tr>
                         <TableCell className='py-3 px-4 text-sm text-zinc-300' colSpan={3}>
-                        Mostrando 10 de 228 itens
+                        Mostrando 10 de {attendees.length} itens
                         </TableCell>
                         <TableCell className='py-3 px-4 text-sm text-zinc-300 text-right' colSpan={3}>
                             <div className='inline-flex items-center gap-8'>
-                                <span>Página 1 de 23</span>
+                                <span>Página {page} de {totalPages} </span>
                           
 
                             <div className='flex gap-1.5 '>
-                                    <IconButton>
+                                    <IconButton onClick={goToFirstPage} disabled={page === 1}>
                                         <ChevronsLeft className='size-4' />
                                     </IconButton>
 
-                                    <IconButton>
+                                    <IconButton onClick={goToPreviousPage} disabled={page === 1}>
                                         <ChevronLeft className='size-4' />
                                     </IconButton>
 
-                                    <IconButton>
+                                    <IconButton onClick={goToNextPage} disabled={page === totalPages}>
                                         <ChevronRight className='size-4' />
                                     </IconButton>
 
-                                    <IconButton>
+                                    <IconButton onClick={goToLastPage} disabled={page === totalPages}>
                                         <ChevronRight className='size-4' />
                                      </IconButton>
                                     </div>
